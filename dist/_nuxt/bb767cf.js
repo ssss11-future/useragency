a1981f7d16eb5891c2f48a2125e5daa86ec35a1e
steve bignell <steve.bignell@gmail.com>
2022-11-01 22:00:54 +1100

    setup

--- dist/_nuxt/bb767cf.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{362:function(n,t,o){"use strict";o.r(t);var e={name:"Blog"},l=o(70),component=Object(l.a)(e,(function(){this._self._c;return this._m(0)}),[function(){var n=this,t=n._self._c;return t("div",[t("h1",[n._v("My first blog post")]),n._v(" "),t("span",[n._v("Hooray!")])])}],!1,null,null,null);t.default=component.exports}}]);
